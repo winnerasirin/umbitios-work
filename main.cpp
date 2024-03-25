179e2677eef536c7bc44b3adeaf0b8b0ef65407a
Fritz Hanspeter <164896613+winnerasirin@users.noreply.github.com>
2024-03-25 16:27:35 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+bool LoadTestNum(ifstream &source, int test_num,
+  Paths64 &subj, Paths64 &subj_open, Paths64 &clip,
+  int64_t& area, int64_t& count, ClipType &ct, FillRule &fr)
+{
+  string line;
+  area = 0; count = 0;
+  if (test_num <= 0) test_num = 1;
+  source.seekg(0, ios_base::beg);
+  subj.clear(); subj_open.clear(); clip.clear();
+
+  while (getline(source, line))
+  {
+    if (test_num)
+    {
+      if (line.find("CAPTION:") != string::npos) --test_num;
+      continue;
+    }
+
+    if (line.find("CAPTION:") != string::npos) break; // ie don't go beyond current test
+
+    else if (line.find("INTERSECTION") != string::npos)
+      ct = ClipType::Intersection;
+    else if (line.find("UNION") != string::npos)
+      ct = ClipType::Union;
+    else if (line.find("DIFFERENCE") != string::npos)
+      ct = ClipType::Difference;
+    else if (line.find("XOR") != string::npos)
+      ct = ClipType::Xor;
